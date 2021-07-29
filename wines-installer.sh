ffeccc7def94016e5451d803576b2ce6a9101ff4
Gendou <rokubungi510@gmail.com>
2021-07-29 22:46:38 +0900

    add new file

---
+++ wines-installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo apt-get update
+sudo dpkg --add-architecture i386
+
+read -p "Can I install wine  ? (y/n): " answer
+case "$answer" in
+	[yY]) sudo apt install wine ;;
+	[nN]) echo "Finish" ;;
+	*) echo "Please Push y or n key."
+esac
+
+read -p "Can I install winetricks ? (y/n)" answer
+case "$answer" in
+	[yY]) 
+	cd "${HOME}/Downloads"
+	wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+	chmod +x winetricks
+	sudo cp winetricks /usr/local/bin ;;
+	[nN]) echo "Finish" ;;
+	*) echo "Please  Push y or n key."
+esac
+
+read -p "
+Would you need install  To start  Aviutl.
+Can I install  same software ?(y/n) 
+* this action need  finish install Aviutl. " answer
+case "$answer" in
+	[yY])
+	WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg
+	WINEPREFIX=~/win64 winecfg
+	WINEPREFIX=$HOME/wine32 winetricks allfonts
+	export WINEPREFIX=$HOME/wine32
+	export WINEARCH=win32
+	LANG=ja_JP.utf8 wine aviutl.exe
+	winetricks dotnet45;;
+	[nN]) echo "Finish";;
+	*) echo "Please  Push y or n key."
+esac
+

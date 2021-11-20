0e6fdbf0be359bf6002baf54e3ff68de548095c6
Rokubungi Gendou <rokubungi510@gmail.com>
2021-11-20 17:39:02 +0900

    Update wines-installer.sh
    .netのインストールをできるように再構成し日本語のガイドに変更。

--- wines-installer.sh
+++ wines-installer.sh
@@ -2,39 +2,36 @@
 sudo apt-get update
 sudo dpkg --add-architecture i386
 
-read -p "Can I install wine  ? (y/n): " answer
+read -p "Wineをインストールしますか? (y/n): " answer
 case "$answer" in
 	[yY]) sudo apt install wine ;;
 	[nN]) echo "Finish" ;;
 	*) echo "Please Push y or n key."
 esac
 
-read -p "Can I install winetricks ? (y/n)" answer
+read -p "winetricksをいんすとーるしますか ? (y/n)" answer
 case "$answer" in
 	[yY]) 
+	sudo apt install winetricks
 	cd "${HOME}/Downloads"
 	wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 	chmod +x winetricks
-	sudo cp winetricks /usr/local/bin 
-	winetricks fakejapanese_ipamona;;
-	[nN]) echo "Finish" ;;
-	*) echo "Please  Push y or n key."
+	sudo cp winetricks /usr/local/bin ;;
+	[nN]) echo "終了" ;;
+	*) echo "ｙかｎのキーを押してよ."
 esac
 
-read -p "
-Would you need install  To start  Aviutl.
-Can I install  same software ?(y/n) 
-* this action need  finish install Aviutl. " answer
+read -p "Aviutlを起動させるための設定を行いますか？" answer
 case "$answer" in
 	[yY])
+	winetricks fakejapanese_ipamona
+	winetricks dotnet452
 	WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg
 	WINEPREFIX=$HOME/wine32 winetricks allfonts
 	WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg
 	export WINEPREFIX=$HOME/wine32
-	export WINEARCH=win32
-	LANG=ja_JP.utf8 wine aviutl.exe
-	winetricks dotnet45;;
-	[nN]) echo "Finish";;
-	*) echo "Please  Push y or n key."
+	export WINEARCH=win32;;
+	[nN]) echo "終了";;
+	*) echo "ｙかｎのキーを押してよ."
 esac
 

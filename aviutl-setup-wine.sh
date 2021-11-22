193757c10055c96623769804a1ef60dfa9fa199d
Rokubungi Gendou <rokubungi510@gmail.com>
2021-11-22 23:33:05 +0900

    Update aviutl-setup-wine.sh

--- aviutl-setup-wine.sh
+++ aviutl-setup-wine.sh
@@ -11,8 +11,8 @@ case "$answer" in
 	　　　　sudo cp winetricks /usr/local/bin
               sudo apt-get install winetricks
 	      winecfg
-	     　WINEPREFIX=~/.NET45-2 winetricks dotnet452
-　　　　　　　　　WINEPREFIX=~/.NET45-2　winetricks fakejapanese_ipamona
+	      winetricks dotnet452
+　　　　　　　　 winetricks fakejapanese_ipamona
 	      winetricks dotnet452;;
 	[nN]) echo "終了" ;;
 	*) echo "ｙかｎのキーを押してよ"
@@ -21,7 +21,6 @@ esac
 read -p "aviutlのセットアップを行います。※wineの設定(windowsバージョン)はxpにしてください。(y/n)" answer
 case "$answer" in
 	[yY]) 
-	winetricks fakejapanese_ipamona
 	wget http://spring-fragrance.mints.ne.jp/aviutl/aviutl110.zip
 	wget http://spring-fragrance.mints.ne.jp/aviutl/exedit93rc1.zip
 	wget https://gwdhla.dm.files.1drv.com/y4mSzKeZxrlo7VJZeAYXkbtM4OtK3r7L8PqzXhLVXVFkKzYe3J96cjwLA6MHUynyKaPqQahP_qb4sgje9NSoXZFfe8rVtmK7VHV9deOdShrqljJFW1_IPrw9cMgaNA9vH_97KWbHAKVrs3tc_BCwyiY3SnY0HPWfx-nOEnaqC0rz3MEyv7ec8qahAKw3hhBgAAZ/x264guiEx_2.68.zip
@@ -37,14 +36,13 @@ case "$answer" in
 	cp lwcolor.auc lwdumper.auf lwinput.aui lwmuxer.auf ../aviutl110/Plugins
 	cd ../
 	cp -r aviutl110 デスクトップ/
-	WINEPREFIX=~/.NET45-2 winecfg
-	WINEPREFIX=~/.NET45-2 wine x264guiEx_2.68/auo_setup.exe
+	wine x264guiEx_2.68/auo_setup.exe
         WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg 
         WINEPREFIX=$HOME/wine32 winetricks allfonts
         WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg 
         export WINEPREFIX=$HOME/wine32
         export WINEARCH=win32
-	rm -r .NET45-2　x264guiEx_2.68 aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_2.68.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip
+	rm -r x264guiEx_2.68 aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_2.68.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip
 	firefox https://aviutl.info/l-smash-works/#toc5;;
 	[nN]) echo "終了" ;;
 	*) echo "ｙかｎのキーを押してよ"

8913e41a2cff739984fa820d960ed11eea54dd50
Rokubungi Gendou <rokubungi510@gmail.com>
2021-11-28 18:51:48 +0900

    Update wines-installer.sh

--- wines-installer.sh
+++ wines-installer.sh
@@ -21,17 +21,27 @@ case "$answer" in
 	*) echo "ｙかｎのキーを押してよ."
 esac
 
-read -p "Aviutlを起動させるための設定を行いますか？" answer
+read -p "Aviutlとx264guiExのセットアップを行いますか？(y/n)" answer
 case "$answer" in
 	[yY])
-	winetricks fakejapanese_ipamona
-	winetricks dotnet452
-	WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg
+	wget https://gwdhla.dm.files.1drv.com/y4mSzKeZxrlo7VJZeAYXkbtM4OtK3r7L8PqzXhLVXVFkKzYe3J96cjwLA6MHUynyKaPqQahP_qb4sgje9NSoXZFfe8rVtmK7VHV9deOdShrqljJFW1_IPrw9cMgaNA9vH_97KWbHAKVrs3tc_BCwyiY3SnY0HPWfx-nOEnaqC0rz3MEyv7ec8qahAKw3hhBgAAZ/x264guiEx_2.68.zip
+	sudo apt install unar -y
+	unar x264guiEx_2.68.zip
+	WINEARCH=win32 WINEPREFIX=$HOME/wine32 wineboot
 	WINEPREFIX=$HOME/wine32 winetricks allfonts
-	WINEARCH=win32 WINEPREFIX=$HOME/win32 winecfg
-	export WINEPREFIX=$HOME/wine32
-	export WINEARCH=win32;;
+	WINEPREFIX=$HOME/wine32 winetricks fakejapanese_ipamona
+	WINEPREFIX=$HOME/wine32 winetricks dotnet452
+	WINEARCH=win32 WINEPREFIX=$HOME/wine32 winecfg
+	WINEPREFIX=$HOME/.wine32 wine x264guiEx_2.68/auo_setup.exe;;
 	[nN]) echo "終了";;
+	*) echo "ｙかｎのキーを押してよ."
+esac
+
+read -p "使用したフォルダを削除しますか? (y/n)" answer
+case "$answer" in
+	[yY]) 
+	rm -r x264guiEx_2.68;;
+	[nN]) echo "終了" ;;
 	*) echo "ｙかｎのキーを押してよ."
 esac
 

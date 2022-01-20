6c9bb022a6d555f9ca3d72d4a9d00386686aab60
Rokubungi Gendou <rokubungi510@gmail.com>
2022-01-20 16:27:11 +0900

    Update aviutl-setup-wine.sh

--- aviutl-setup-wine.sh
+++ aviutl-setup-wine.sh
@@ -2,19 +2,13 @@
 sudo apt-get update
 sudo dpkg --add-architecture i386
 
-read -p "Ubuntu18.04,Ubuntu 19.04,Ubuntu 19.10ですか? 依存関係にあるFAudioをインストールします。(y/n): " answer
-case "$answer" in
-	[yY])wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/amd64/libfaudio0_19.07-0~bionic_amd64.deb
-	　　　wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/i386/libfaudio0_19.07-0~bionic_i386.deb
-	　　　sudo dpkg -i libfaudio0_19.07-0~bionic_amd64.deb libfaudio0_19.07-0~bionic_i386.deb;;
-	[nN]) echo "終了" ;;
-	*) echo "ｙかｎのキーを押してよ"
-esac
-
 read -p "WineとWinetricksをインストールしますか? (y/n): " answer
 case "$answer" in
-	[yY]) sudo apt install wine  -y
-              sudo apt-get install winetricks -y
+	[yY]) wget -nc https://dl.winehq.org/wine-builds/winehq.key
+	      sudo apt-key add winehq.key
+	      sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
+	      sudo apt install --install-recommends winehq-stable
+	      sudo apt install winetricks
 	      winecfg
 	      WINEARCH=win32 WINEPREFIX=$HOME/.wine32 wineboot;;
 	[nN]) echo "終了" ;;
@@ -24,7 +18,6 @@ esac
 read -p "aviutlのセットアップを行います。※wineの設定(windowsバージョン)はxpにしてください。(y/n)" answer
 case "$answer" in
 	[yY]) 
-	cd "${HOME}/Downloads"
 	wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 	chmod +x winetricks
 	sudo cp winetricks /usr/local/bin
@@ -49,7 +42,8 @@ case "$answer" in
 	WINEPREFIX=$HOME/.wine32 winetricks fakejapanese_ipamona
 	WINEPREFIX=$HOME/.wine32 wine x264guiEx_2.68/auo_setup.exe
 	firefox https://aviutl.info/l-smash-works/#toc5
-	rm -r x264guiEx_2.68 aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_2.68.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip;;
+	rm -r aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_2.68.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip
+	wine /home/shuta/デスクトップ/aviutl110/aviutl.exe;;
 	[nN]) echo "終了" ;;
 	*) echo "ｙかｎのキーを押してよ"
 esac

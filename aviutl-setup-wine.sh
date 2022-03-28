b9e285ed7241e42026b46f1591f8a156912fa2f0
Gendou <72452449+gendousite@users.noreply.github.com>
2022-03-29 02:06:04 +0900

    インストール方法の修正
    インストール方法や諸々の変更点が発生したため修正しました。

--- aviutl-setup-wine.sh
+++ aviutl-setup-wine.sh
@@ -2,48 +2,49 @@
 sudo apt-get update
 sudo dpkg --add-architecture i386
 
-read -p "WineとWinetricksをインストールしますか? (y/n): " answer
+read -p "WineとWinetricksをインストールしますか? *ubuntu20.04に対応しStaging branchをインストールします。(y/n): " answer
 case "$answer" in
-	[yY]) wget -nc https://dl.winehq.org/wine-builds/winehq.key
-	      sudo apt-key add winehq.key
-	      sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
-	      sudo apt install --install-recommends winehq-stable
-	      sudo apt install winetricks
-	      winecfg
-	      WINEARCH=win32 WINEPREFIX=$HOME/.wine32 wineboot;;
+	[yY]) 
+	wget -nc https://dl.winehq.org/wine-builds/winehq.key
+	sudo apt-key add winehq.key
+	sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
+	sudo apt update
+	sudo apt install --install-recommends winehq-staging
+	wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+	chmod +x winetricks;;
 	[nN]) echo "終了" ;;
 	*) echo "ｙかｎのキーを押してよ"
 esac
 
-read -p "aviutlのセットアップを行います。※wineの設定(windowsバージョン)はxpにしてください。(y/n)" answer
+read -p "wine用にフォントをインストールしますか？(y/n): " answer
 case "$answer" in
-	[yY]) 
-	wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-	chmod +x winetricks
-	sudo cp winetricks /usr/local/bin
+	[yY])
+	winetricks fakejapanese_ipamona
+	winetricks fakejapanese_vlgothic
+	winetricks corefonts;;
+	[nN]) echo "終了" ;;
+	*) echo "ｙかｎのキーを押してよ"
+esac
+
 	wget http://spring-fragrance.mints.ne.jp/aviutl/aviutl110.zip
 	wget http://spring-fragrance.mints.ne.jp/aviutl/exedit93rc1.zip
-	wget https://gwdhla.dm.files.1drv.com/y4mSzKeZxrlo7VJZeAYXkbtM4OtK3r7L8PqzXhLVXVFkKzYe3J96cjwLA6MHUynyKaPqQahP_qb4sgje9NSoXZFfe8rVtmK7VHV9deOdShrqljJFW1_IPrw9cMgaNA9vH_97KWbHAKVrs3tc_BCwyiY3SnY0HPWfx-nOEnaqC0rz3MEyv7ec8qahAKw3hhBgAAZ/x264guiEx_2.68.zip
 	wget https://pop.4-bit.jp/bin/l-smash/L-SMASH_Works_r940_plugins.zip
+	wget https://github.com/rigaya/x264guiEx/releases/download/3.03/x264guiEx_3.03.zip
 	sudo apt install unar -y
 	unar aviutl110.zip
 	unar exedit93rc1.zip
-	unar x264guiEx_2.68.zip
+	unar x264guiEx_3.03.zip
 	unar L-SMASH_Works_r940_plugins.zip
 	cp -pR exedit93rc1/* aviutl110/
 	mkdir aviutl110/Plugins
 	cd  L-SMASH_Works_r940_plugins
 	cp lwcolor.auc lwdumper.auf lwinput.aui lwmuxer.auf ../aviutl110/Plugins
-	cd ../
+	cd ..
+	cd x264guiEx_3.03
+	cp -r exe_files x264guiEx_readme.txt ../aviutl110
+	cd plugins
+	cp -r x264guiEx_stg auo_setup.auf x264guiEx.ini x264guiEx.auo ../../aviutl110/Plugins
+	cd ../../
 	cp -r aviutl110 デスクトップ/
-	WINEARCH=win32 WINEPREFIX=$HOME/.wine32 winecfg
-	WINEPREFIX=$HOME/.wine32 winetricks allfonts
-	WINEPREFIX=$HOME/.wine32 winetricks dotnet452
-	WINEPREFIX=$HOME/.wine32 winetricks fakejapanese_ipamona
-	WINEPREFIX=$HOME/.wine32 wine x264guiEx_2.68/auo_setup.exe
-	firefox https://aviutl.info/l-smash-works/#toc5
-	rm -r aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_2.68.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip
-	wine /home/shuta/デスクトップ/aviutl110/aviutl.exe;;
-	[nN]) echo "終了" ;;
-	*) echo "ｙかｎのキーを押してよ"
-esac
+	rm -r aviutl110 exedit93rc1 L-SMASH_Works_r940_plugins x264guiEx_3.03 x264guiEx_3.03.zip aviutl110.zip exedit93rc1.zip L-SMASH_Works_r940_plugins.zip
+	wine デスクトップ/aviutl110/aviutl.exe
